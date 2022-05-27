dc65c7fdc379c5d74491bb0b259d4132078f0197
mghmay <mghmay@gmail.com>
2022-05-27 11:41:15 +0100

    add post slice with reducer and reddit async fetch all posts

--- src/features/Posts/postsSlice.js
+++ src/features/Posts/postsSlice.js
@@ -1,23 +1,52 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import Reddit from "../../util/Reddit";
+
+export const fetchPosts = createAsyncThunk(
+	"frontPage/fetchFrontPage",
+	async () => {
+		const response = await Reddit.fetchFrontPage();
+		return response;
+	}
+);
 
 const initialState = {
-    posts: {
-    },
-    before: "",
-    after: "",
-    loading: false,
-    error: false,
+	posts: {},
+	before: "",
+	after: "",
+	// My thinking here is that the status doesn't need to be a loading Boolean, it can equally be a string to reflect the status of the fetch request
+	status: "idle", // 'idle' || 'loading' || 'succeeded' || 'failed'
+	// Same with the error status, it doesn't need to be a boolean, it can be a message which is the action.payload of the error message.
+	error: null,
 };
 
 const postsSlice = createSlice({
-  name: "posts",
-  initialState,
-  reducers: {
-
-  }
+	name: "posts",
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchPosts.pending, (state) => {
+				state.status = "loading";
+			})
+			.addCase(fetchPosts.fulfilled, (state, action) => {
+				state.status = "succeeded";
+				state.posts.posts = action.payload.children;
+				state.nextPage = action.payload.after;
+				state.prevPage = action.payload.before;
+			})
+			.addCase(fetchPosts.rejected, (state, action) => {
+				state.status = "failed";
+				state.error = action.error.message;
+			});
+	},
 });
 
-// export const selectAllPosts = (state) => state.cards.cards;
-// export const { functions } = cardsSlice.actions;
+export const selectAllPosts = (state) => state.posts.posts;
+
+export const selectPostsStatus = (state) => state.posts.status;
+
+export const selectPostsError = (state) => state.posts.error;
+
+export const {} = postsSlice.actions;
 
 export default postsSlice.reducer;
