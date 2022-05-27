2fd513f6a006d0a40c9ff95e2336bc17e293efa5
mghmay <mghmay@gmail.com>
2022-05-27 10:34:27 +0100

    add __test__ folder and move App.test.js to test folder

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "../src/app/App";
+import { store } from "../app/store";
+import App from "../app/App";
 import "@testing-library/jest-dom";
 
 test("use jsdom in this test file", () => {
 	const element = document.createElement("div");
 	expect(element).not.toBeNull();
 });
+
 test("renders hello h1", () => {
 	const { getByText } = render(
 		<Provider store={store}>
 			<App />
 		</Provider>
 	);
-
 	expect(getByText(/hello/i)).toBeInTheDocument();
 });
