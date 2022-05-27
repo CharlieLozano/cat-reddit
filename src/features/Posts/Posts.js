1b9b098e9a65a1c3ebff683bd4f17b78b987d20f
mghmay <mghmay@gmail.com>
2022-05-27 11:48:46 +0100

    add Posts selectors and dispatch with use effect

--- src/features/Posts/Posts.js
+++ src/features/Posts/Posts.js
@@ -1,4 +1,24 @@
-export default function Posts() {
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	selectAllPosts,
+	selectPostsStatus,
+	selectPostsError,
+} from "./postsSlice";
 
-  return ();
-}
+const FrontPage = () => {
+	const dispatch = useDispatch();
+	const allPosts = useSelector(selectAllPosts);
+	const postsStatus = useSelector(selectPostsStatus);
+	const postsError = useSelector(selectPostsError);
+
+	useEffect(() => {
+		if (postsStatus === "idle") {
+			dispatch(fetchFrontPagePosts());
+		}
+	}, [dispatch, postsStatus]);
+
+	return <div>FrontPage</div>;
+};
+
+export default FrontPage;
