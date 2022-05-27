6ae9f026920016da321ba841cdea3c034539aeb0
mghmay <mghmay@gmail.com>
2022-05-27 10:29:23 +0100

    add testing-library with jest-dom. 2 working tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "../src/app/App";
+import "@testing-library/jest-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+test("use jsdom in this test file", () => {
+	const element = document.createElement("div");
+	expect(element).not.toBeNull();
+});
+test("renders hello h1", () => {
+	const { getByText } = render(
+		<Provider store={store}>
+			<App />
+		</Provider>
+	);
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+	expect(getByText(/hello/i)).toBeInTheDocument();
 });
