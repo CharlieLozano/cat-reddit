5de593f8d970b2e250b04540178b1fba6ca43278
mghmay <mghmay@gmail.com>
2022-05-28 12:30:42 +0100

    add comments

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// get the props from the Posts feature. I thought this would be the best way to do it so that this component stays stateless and we can reuse it in multiple different parts of the app but we could also go through the store, let me know what you think.
 const Post = (props) => {
 	const { title, url, subreddit, selftext } = props;
 	return (
