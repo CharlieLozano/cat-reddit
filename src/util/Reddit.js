c0577f734371624c54b8975a90887142b40f0ce8
CharlieLozano <caolort@hotmail.com>
2022-05-31 10:35:21 -0500

    Completed the fetchHomePage and fetchPostPage

--- src/util/Reddit.js
+++ src/util/Reddit.js
@@ -12,15 +12,92 @@ const getJson = async (url, errorMsg = "Hmm something went wrong") => {
 
 // Reddit componenent
 const Reddit = {
-	async fetchHomePage() {
+	async fetchHomePage(searchTerm, filters, after) {
+
+		// Defining the url for the fetch
+		const url = `https://www.reddit.com/user/outside-research4792/m/cats/search.json?q=`
+		let endpoint = url + searchTerm
+		if(after){
+			endpoint = `${endpoint}&after=${after}`
+		}
+
 		try {
-			const response = await getJson(MULTI_PATH);
-			return response.data;
+			//Fetching and making a copy of the childrens since it will be a destructive process
+			const response = await getJson(endpoint);
+			let children = [...response.data.children]
+			
+			// Filter by format
+			const formats = ['png', 'jpg']
+			formats.forEach(format => {
+				children = children.filter(child => child.data.thumbnail.includes(format))
+			});
+
+			// Filter by filters
+			filters.forEach(filter => {
+				children = children.filter(child => child.data.title.includes(filter))
+			});
+
+
+			// Make the listing array of objects
+			const listings = []
+
+			children.forEach(child => {
+				const short = child.data
+				listings.push({
+					id: short.id,
+					url: short.permalink,
+					author: short.author,
+					title: short.title,
+					thumbnail: short.thumbnail,
+					created: short.created
+				})
+			})
+
+			// Returning and object with two keys
+			return {after: response.after, listings: listings};
 		} catch (err) {
 			return err.message;
 		}
 	},
-	async fetchPostPage() {},
+	async fetchPostPage(id) {
+		const endpoint = `https://www.reddit.com/r/${subreddit}/comments/${id}.json`
+		
+		try {
+			const response = await getJson(endpoint);
+
+			// Creating Post Object
+			let short = response[0].data.children[0].data
+
+			const post = {
+				id: short.id,
+				url: short.permalink,
+				author: short.author,
+				title: short.title,
+				thumbnail: short.thumbnail,
+				created: short.created
+			}
+
+			// Creating Comments Array
+			const comments = response[1].data.children
+			const newComments = []
+
+			comments.forEach(comment =>{
+				short = comment.data
+				newComments.push({
+					id: short.id,
+					author: short.author,
+					body: short.body,
+					ups: short.ups,
+					created: short.created 
+				})
+			})
+
+			// Returning and object with two keys		
+			return {post: post, comments: newComments};
+		} catch (err) {
+			return err.message;
+		}
+	},
 };
 
 export default Reddit;
