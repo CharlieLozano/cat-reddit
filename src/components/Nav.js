44373733b0b1c18e0dd7ca9a73dad7d338b232af
CharlieLozano <caolort@hotmail.com>
2022-06-07 11:31:57 -0500

    Added style to Nav Component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,6 +6,7 @@ export default function Nav() {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("")
   const [subreddit, setSubreddit] = useState("None")
+  const [subsSelection, setSubsSelection] = useState("hidden")
   
   const handleSearch = (event) => {
     const value = event.target.value
@@ -19,51 +20,57 @@ export default function Nav() {
     dispatch(fetchPosts({searchTerm: searchTerm, subreddit: value, after: "initial"}));
   }
 
-  return (
-    <form>
-      <input type="text" name="searchTerm" value={searchTerm} onChange={handleSearch}/>
-      <div className="subreddits-container">
+  const toggleSubreddits = (event) => {
+    event.preventDefault();
+    subsSelection == 'hidden' ? setSubsSelection('show') : setSubsSelection('hidden')
+  }
+
+  const subredditsContainer = () => {
+      return (
+        <div className="subreddits-container">
         <label>
           <input type="radio" id="" name="subreddit" value="None" onChange={handleCheckbox} checked={subreddit === 'None'} />
-          None
+          <span className='subreddit-none'>None</span>
         </label>
         <label>
           <input type="radio" id="" name="subreddit" value="Kitten" onChange={handleCheckbox} checked={subreddit === 'Kitten'}/>
-          r/Kitten
+          <span className='subreddit-r'>r/</span>Kitten
         </label>
         <label>
           <input type="radio" id="" name="subreddit" value="SupermodelCats"  onChange={handleCheckbox} checked={subreddit === 'SupermodelCats'}/>
-          r/SupermodelCats
+          <span className='subreddit-r'>r/</span>SupermodelCats
         </label>
         <label>
           <input type="radio" id="" name="subreddit" value="blackcats"  onChange={handleCheckbox} checked={subreddit === 'blackcats'}/>
-          r/blackcats
+          <span className='subreddit-r'>r/</span>blackcats
         </label>
         <label>
           <input type="radio" id="" name="subreddit" value="catsAreAssholes"  onChange={handleCheckbox} checked={subreddit === 'catsAreAssholes'}/>
-          r/catsAreAssholes
+          <span className='subreddit-r'>r/</span>catsAreAssholes
         </label>
         <label>
           <input type="radio" id="" name="subreddit" value="curledfeetsies"  onChange={handleCheckbox} checked={subreddit === 'curledfeetsies'}/>
-          r/curledfeetsies
+          <span className='subreddit-r'>r/</span>curledfeetsies
         </label>
         <label>
           <input type="radio" id="" name="subreddit" value="CatsStandingUp" onChange={handleCheckbox} checked={subreddit === 'CatsStandingUp'}/>
-          r/CatsStadingUp
+          <span className='subreddit-r'>r/</span>CatsStadingUp
         </label>
         <label>
           <input type="radio" id="" name="subreddit" value="ragdolls" onChange={handleCheckbox} checked={subreddit === 'ragdolls'}/>
-          r/ragdolls
+          <span className='subreddit-r'>r/</span>ragdolls
         </label>
-
-
-
-
-        
-        
-        
-        
       </div>
+      )
+  }
+
+  return (
+    <form>
+      <input className="search-bar" type="text" name="searchTerm" value={searchTerm} onChange={handleSearch}/>
+      <button className="subreddits-btn" onClick={toggleSubreddits}>
+      Subreddits
+      </button>
+      { subsSelection == 'show' ? subredditsContainer() : undefined}
     </form>
   );
 }
